111cb8c6f84605737a455d6263b1843fda9c7357
Julius Žaromskis <julius@ignitenet.com>
2019-03-25 17:06:34 +0200

    We do add build to git, because it will be served over github pages

---
+++ react_demo/build/precache-manifest.e509a3a9d03b785e964d9a2ffb0a4e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a91ed3854e916825b335",
+    "url": "/static/js/main.63d88c66.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "a91ed3854e916825b335",
+    "url": "/static/css/main.b74cc1f8.chunk.css"
+  },
+  {
+    "revision": "b86912f94935ce4e9a37bb251eb105e8",
+    "url": "/index.html"
+  }
+];
