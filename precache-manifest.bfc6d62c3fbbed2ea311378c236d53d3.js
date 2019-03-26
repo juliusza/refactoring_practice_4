7ecc8db2fe306c407613ed4e00d76717ffdba81a
Julius Žaromskis <julius@ignitenet.com>
2019-03-26 10:51:40 +0100

    Updates

---
+++ precache-manifest.bfc6d62c3fbbed2ea311378c236d53d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a2316504c30c587cbee",
+    "url": "/react_demo/static/js/runtime~main.27d5aae1.js"
+  },
+  {
+    "revision": "2eda4359782a046fc61e",
+    "url": "/react_demo/static/js/main.c372c665.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/react_demo/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "2eda4359782a046fc61e",
+    "url": "/react_demo/static/css/main.9f8f8ebf.chunk.css"
+  },
+  {
+    "revision": "6f0c3e966b745fd7447fad71bacc6fb0",
+    "url": "/react_demo/index.html"
+  }
+];
